7f2aa3966dde866faefa9b6220c48951e05bcc96
Vitaliy Fomin <fomin_v.v@mail.ru>
2024-02-24 15:35:06 +0500

    task03

--- lesson02/src/task03/Expr.java
+++ lesson02/src/task03/Expr.java
@@ -1,19 +1,16 @@
 package task03;
-
 class Expr {
 
     public static double expr(int a, int b) {
+        double result = 0.0; // Initialize the result
         if (b != 0) {
-            return (double) a / b;
-        } else {
-            System.out.println("Невозможно выполнить операцию деления на ноль.");
-            return 0;
+            result = (double) a / b; // Perform the division and store the result
         }
+        printSum(a, b);
+        return result; // Return the result
     }
 
     public static void printSum(int a, int b) {
-        int sum = a + b;
-        System.out.println(sum);
+        System.out.println(a + b);
     }
 }
-
--- lesson02/src/task03/Printer.java
+++ lesson02/src/task03/Printer.java
@@ -1,5 +1,4 @@
 package task03;
-
 public class Printer {
     public static void main(String[] args) {
         int a;
@@ -7,7 +6,7 @@ public class Printer {
 
         if (args.length == 0) {
             a = 90;
-            b = 3; // Значения по умолчанию, если аргументы не переданы через командную строку
+            b = 3; // Default values if no arguments are provided
         } else {
             a = Integer.parseInt(args[0]);
             b = Integer.parseInt(args[1]);
@@ -15,6 +14,5 @@ public class Printer {
 
         double result = Expr.expr(a, b);
         System.out.println(result);
-        Expr.printSum(a, b);
     }
 }
